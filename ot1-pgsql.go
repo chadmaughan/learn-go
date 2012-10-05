5e1b88e9e352644c2fa096793539a896d6c0bc63
Chad Maughan <chadmaughan@gmail.com>
2012-10-05 10:19:40 -0600

    psql example

---
+++ ot1-pgsql.go
@@ -0,0 +1,32 @@
+// go get github.com/bmizerany/pq
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+import (
+	_ "github.com/lxn/go-pgsql"
+)
+
+func main() {
+
+	// connect to database
+	db, err := sql.Open("postgres", "user=chad password= host=localhost port=5432 dbname=temperature sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	var msg string
+
+	err = db.QueryRow("SELECT $1 || ' ' || $2;", "Hello", "SQL").Scan(&msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(msg)
+}
